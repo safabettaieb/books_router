3b4f0094a318c90851bba79bf404aac2cf525bf1
Safa Bettaieb <safa.bettaieb1@gmail.com>
2020-03-01 23:13:08 +0100

    create books routers and get all books

--- OneDrive/Bureau/GLSI-a_semester2/NodeJS/books_authors/index.js
+++ OneDrive/Bureau/GLSI-a_semester2/NodeJS/books_authors/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const morgan =require('morgan');
 const index_router = require('./routers/index_router');
-
+const books_router = require('./routers/books_router');
 
 const app = express();
-app.use('',index_router);
 const port = process.env.PORT || 3000 ;
 
 /*middleware*/
@@ -12,7 +11,7 @@ if(app.get('env') ==='development'){
     app.use(morgan('tiny'))  }
 
 /* routes */
-
-
+app.use('',index_router);
+app.use('/api/books/',books_router); //  applicquer sur tous le ficher
 /*server*/
 app.listen(port, () => console.log(`Listening on ${port} .....`));
---
+++ OneDrive/Bureau/GLSI-a_semester2/NodeJS/books_authors/routers/books_router.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const joi =require('joi')
+
+module.exports = router ;
+ /* data */
+var books = [{
+ id : 1 ,
+ name : "books 1 " ,
+ description : "description book 1" ,
+ imageUrl : "http://www.google.com/img1" ,
+ author : {
+     firstName : "firstname1",
+     lastName :"lastname1" ,
+     nbBooks : 10 ,
+ } ,
+ sources : ["source 1 ","source 2 " ,"source 3"]
+} ,
+{
+    id : 2 ,
+    name : "books 2 " ,
+    description : "description book 2" ,
+    imageUrl : "http://www.google.com/img 2" ,
+    author : {
+        firstName : "firstname 2",
+        lastName :"lastname 2" ,
+        nbBooks : 60 ,
+    } ,
+    sources : ["source 1 ","source 2 " ,"source 3"]
+   }
+]
+
+router.get('/', (req, res) => { // préfixe => index 
+    res.status(200)
+    res.json(books)
+  });
+  
+
+
+module.exports = router ;
