203ddace5ce55d1fee68db343d1799e77c7623b8
Safa Bettaieb <safa.bettaieb1@gmail.com>
2020-03-01 22:00:17 +0100

    create index_router

--- OneDrive/Bureau/GLSI-a_semester2/NodeJS/books_authors/index.js
+++ OneDrive/Bureau/GLSI-a_semester2/NodeJS/books_authors/index.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const morgan =require('morgan');
+const index_router = require('./routers/index_router');
+
 
 const app = express();
+app.use('',index_router);
 const port = process.env.PORT || 3000 ;
+
 /*middleware*/
 if(app.get('env') ==='development'){
     app.use(morgan('tiny'))  }
+
 /* routes */
-
-
-
-
-
 
 
 /*server*/
