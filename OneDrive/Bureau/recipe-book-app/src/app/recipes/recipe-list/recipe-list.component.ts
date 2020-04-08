261a710040cc8ee01699ac3c9f6e004bfe422a33
Safa Bettaieb <safa.bettaieb1@gmail.com>
2020-04-08 05:38:22 +0100

    display a list of recipes

--- OneDrive/Bureau/recipe-book-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ OneDrive/Bureau/recipe-book-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes : Recipe[] = [
+    new Recipe('fisrt recipe','description test 1','https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg'),
+    new Recipe('second recipe','description test 2 ','https://images-gmi-pmc.edge-generalmills.com/1e24b5e7-e3e3-43ce-b737-a2215397f006.jpg')
+
+  ];
 
   constructor() { }
 
