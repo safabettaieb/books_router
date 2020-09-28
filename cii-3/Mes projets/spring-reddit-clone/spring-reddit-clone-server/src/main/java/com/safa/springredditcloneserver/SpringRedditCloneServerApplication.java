19e6de5dc17ca6518f406ab4e4fa80bdf340bedf
Safa Bettaieb <safa.bettaieb1@gmail.com>
2020-09-28 17:57:51 +0100

    first commit

---
+++ cii-3/Mes projets/spring-reddit-clone/spring-reddit-clone-server/src/main/java/com/safa/springredditcloneserver/SpringRedditCloneServerApplication.java
@@ -0,0 +1,13 @@
+package com.safa.springredditcloneserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedditCloneServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRedditCloneServerApplication.class, args);
+	}
+
+}
---
+++ cii-3/Mes projets/spring-reddit-clone/spring-reddit-clone-server/src/test/java/com/safa/springredditcloneserver/SpringRedditCloneServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safa.springredditcloneserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedditCloneServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
